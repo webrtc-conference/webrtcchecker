4fe783cbfa8e94696648d3c68c88e6c6b6f4beeb
Y-NAKA <com.y.naka@gmail.com>
2014-11-14 16:23:25 +0900

    setup project

---
+++ gulp/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var tsc = require('gulp-typescript-compiler');
+var uglify = require('gulp-uglify');
+
+gulp.task('tsc', function () {
+    return gulp.src('../src/*.ts')
+        .pipe(tsc({
+            module: '',
+            sourcemap: false,
+            logErrors: true
+        }))
+        .pipe(gulp.dest('../js/')
+    );
+});
+
+gulp.task('compile-extension',['tsc'], function () {
+    gulp.src('../js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('../js/')
+    );
+});
+
+gulp.task('default',['compile-extension']);
